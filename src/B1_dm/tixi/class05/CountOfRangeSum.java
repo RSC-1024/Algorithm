b88ed9776f5b7dd4beda667cb046c953c492c4d5
madanmei <Mdm....0818>
2022-04-29 08:17:12 +0800

    class 05 count of rangeSum

--- src/B1_dm/tixi/class05/CountOfRangeSum.java
+++ src/B1_dm/tixi/class05/CountOfRangeSum.java
@@ -28,7 +28,45 @@ public class CountOfRangeSum {
         }
         int M=L+(R-L)>>1;
 
-        return  process(sum,L,M,)
+        return  process(sum,L,M,lower,up)+process(sum,M+1,R,lower,up)+merge(sum,L,M,R,lower,up);
 
     }
+
+    //long
+    private static int merge(long[] sum, int L, int M, int R, int lower, int up) {
+        //
+        int windowL=L;
+        int windowR=L;
+        int ans=0;
+        for (int i=M+1; i <= R; i++) {
+            int min= sum[i]-up;
+            int max =sum[i]-lower;
+            while (sum[windowL]<min){
+                windowL++;
+            }
+            while (sum[windowR]<max){
+                windowR++;
+            }
+            ans+=windowR-windowL;
+            long[] help = new long[R - L + 1];
+            int p = 0;
+            int p1 = L;
+            int p2 = M + 1;
+            while (p1 <= M && p2 <= R) {
+                help[p++] = sum[p1] <= sum[p2] ? sum[p1++] : sum[p2++];
+            }
+            while (p1 <= M) {
+                help[p++] = sum[p1++];
+            }
+            while (p2 <= R) {
+                help[p++] = sum[p2++];
+            }
+            for (p = 0; p < help.length; p++) {
+                sum[L + p] = help[p];
+            }
+        }
+        //统计
+        //merge
+    }
+
 } 
