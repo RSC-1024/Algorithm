1afe7b536d0dadff4e9903723bb4567ee1c2c4dd
liuxing <liuxing@juzifenqi.com>
2022-07-26 17:21:46 +0800

    堆排序修改

--- src/B2_lx/mytx/class06/Code03_HeapSort.java
+++ src/B2_lx/mytx/class06/Code03_HeapSort.java
@@ -14,14 +14,42 @@ public class Code03_HeapSort {
         if (null == arr || arr.length <2){
             return;
         }
+        // 自上而下的堆排序 
+        // O(N*logN) ：先for循环一遍N,然后 logN调整
         for (int i = 0; i < arr.length; i++) {
             heapInsert(arr,i);
         }
+        // TODO: 2022/7/26 另外一种方法：自下而上的堆排序 
+        
+        int heapSize = arr.length;
+        swap(arr,0,--heapSize);
+        while (heapSize>0){
+            heapify(arr,0,heapSize);
+            swap(arr,0,--heapSize);
+        }
     }
     public static void heapInsert(int[] arr,int index){
+        // 如果当前节点 > 父亲节点
+        // 交换 
+        // 变更当前节点=父亲节点
         while (arr[index] > arr[(index-1)/2]){
             swap(arr,index,(index-1)/2);
             index = (index-1)/2;
+        }
+    }
+    public static void heapify(int[] arr,int index,int heapSize){
+        // 有左孩子
+        int left = index*2+1;
+        while (left < heapSize){
+            // 找出index的孩子 中最大的一个
+            int largest = left+1 < heapSize && arr[left+1]>arr[left] ? left+1 :left;
+            
+            if (arr[index] < arr[largest]){
+                swap(arr,index,largest);
+                left = largest*2+1;
+            }else {
+                break;
+            }
         }
     }
     private static void swap(int[] arr, int i, int j){
